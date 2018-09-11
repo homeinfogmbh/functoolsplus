4cfc3adf774c7ec38f2eb1225bc3f1442b98e205
Richard Neumann <r.neumann@homeinfo.de>
2018-09-11 11:11:26 +0200

    Removed callbackpartial and datetimenow.

--- functoolsplus.py
+++ functoolsplus.py
@@ -1,41 +1,9 @@
 """Even higher-order functions and operations on callable objects."""
 
-from datetime import datetime
 from functools import wraps
-from itertools import chain
-
-__all__ = [
-    'callbackpartial',
-    'datetimenow',
-    'coerce',
-    'wants_instance']
 
 
-def callbackpartial(function, *callbacks, **kwcallbacks):
-    """Returns a partial function with arguments
-    extended by the results of the given callbacks.
-    Keyword arguments explicitely passed to the resulting
-    function may override keywords derived from callbacks.
-    """
-
-    @wraps(function)
-    def wrapper(*args, **kwargs):
-        """Wraps the respective function."""
-        keywords = {name: callback() for name, callback in kwcallbacks.items()}
-        # Allow passed kwargs to override callback kwargs.
-        keywords.update(kwargs)
-        arguments = chain((callback() for callback in callbacks), args)
-        return function(*arguments, **keywords)
-
-    return wrapper
-
-
-def datetimenow(function):
-    """Passes datetime.now() as first
-    argument to the respective function.
-    """
-
-    return callbackpartial(function, datetime.now)
+__all__ = ['coerce', 'wants_instance']
 
 
 def coerce(type_):
