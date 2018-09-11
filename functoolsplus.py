d31b3b4b018e2d8e2c78faa5828b67293e0822ec
Richard Neumann <r.neumann@homeinfo.de>
2018-09-11 11:08:04 +0200

    Removed typecheck.

--- functoolsplus.py
+++ functoolsplus.py
@@ -1,6 +1,5 @@
 """Even higher-order functions and operations on callable objects."""
 
-from collections import namedtuple
 from datetime import datetime
 from functools import wraps
 from itertools import chain
@@ -11,17 +10,7 @@ __all__ = [
     'callbackpartial',
     'datetimenow',
     'coerce',
-    'wants_instance',
-    'typecheck']
-
-
-Mismatch = namedtuple('Mismatch', ('arg', 'value', 'type', 'expected'))
-
-
-class TypeMismatch(TypeError):
-    """Indicates that one or more types did not match."""
-
-    pass
+    'wants_instance']
 
 
 def limit_executions(limit=1):
@@ -102,41 +91,3 @@ def wants_instance(function):
         return args[0] == 'self'
     except IndexError:
         return False
-
-
-def typecheck(*types, **kwtypes):
-    """Decorator factory to specify types
-    expeced by the underlying function.
-    """
-
-    def decorator(function):
-        """Actual decorator function."""
-
-        @wraps(function)
-        def wrapper(*args, **kwargs):
-            """Wrapper function."""
-            mismatches = []
-
-            for position, (typ, arg) in enumerate(zip(types, args)):
-                if typ is None:
-                    continue
-                elif not isinstance(arg, typ):
-                    mismatches.append(Mismatch(position, arg, type(arg), typ))
-
-            for key, value in kwargs.items():
-                try:
-                    typ = kwtypes[key]
-                except KeyError:
-                    continue
-
-                if not isinstance(value, typ):
-                    mismatches.append(Mismatch(key, value, type(value), typ))
-
-            if mismatches:
-                raise TypeMismatch(*mismatches)
-
-            return function(*args, **kwargs)
-
-        return wrapper
-
-    return decorator
