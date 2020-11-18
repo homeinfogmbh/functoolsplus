8ca04bdd53f405b4fc9b557a375f401eba1421bc
Richard Neumann <mail@richard-neumann.de>
2020-11-18 11:16:52 +0100

    Refac.

--- functoolsplus.py
+++ functoolsplus.py
@@ -1,30 +1,47 @@
 """Even higher-order functions and operations on callable objects."""
 
 from functools import wraps
+from sys import exit    # pylint: disable=W0622
+from typing import Callable, Iterable
 
 
 __all__ = ['coerce', 'orderedfrozenset', 'wants_instance']
 
 
-def coerce(type_):
+def coerce(typ: type) -> Callable:
     """Converts the return value into the given type."""
 
-    def decorator(function):
+    def decorator(function: Callable) -> typ:
         """Decorates the given function."""
 
         @wraps(function)
         def wrapper(*args, **kwargs):
             """Wraps the respective function."""
-            result = function(*args, **kwargs)
-            return type_(result)
+            return typ(function(*args, **kwargs))
 
         return wrapper
 
     return decorator
 
 
+def exiting(function: Callable) -> Callable:
+    """Makes a function exit the program with its return code."""
+
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+        """Wraps the respective function."""
+        result = function(*args, **kwargs)
+
+        if result is None:
+            exit(0)
+
+        exit(result)
+
+    return wrapper
+
+
 @coerce(tuple)
-def orderedfrozenset(items=()):
+def orderedfrozenset(items: Iterable = ()):
     """Creates a tuple with unique items."""
 
     processed = set()
@@ -35,7 +52,7 @@ def orderedfrozenset(items=()):
             yield item
 
 
-def wants_instance(function):
+def wants_instance(function: Callable) -> bool:
     """Determines whether the respective function is considered a method."""
 
     try:
