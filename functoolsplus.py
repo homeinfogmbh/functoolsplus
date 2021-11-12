952cb4911f3d5e7afa81ddaefc8bb7ff118c7a59
Richard Neumann <mail@richard-neumann.de>
2021-11-12 10:47:22 +0100

    Migrate to setuptools

--- functoolsplus.py
+++ functoolsplus.py
@@ -1,13 +1,12 @@
 """Even higher-order functions and operations on callable objects."""
 
-from contextlib import suppress
 from datetime import datetime
 from functools import wraps
 from sys import exit, stderr    # pylint: disable=W0622
 from typing import Any, Callable, IO
 
 
-__all__ = ['coerce', 'coroproperty', 'exiting', 'timeit', 'wants_instance']
+__all__ = ['coerce', 'exiting', 'wants_instance']
 
 
 def coerce(typ: type) -> Callable[..., Any]:
@@ -24,56 +23,6 @@ def coerce(typ: type) -> Callable[..., Any]:
         return wrapper
 
     return decorator
-
-
-def coroproperty(method: Callable) -> property:
-    """Decorator for coroutine-based all-in-one getter and setter methods.
-
-    Usage:
-
-        class Circle:
-            '''Information about a circle.'''
-
-            def __init__(self, radius: float):
-                '''Initializes the circle with its radius.'''
-                self.radius = radius
-
-            @coroproperty
-            def diameter(self):
-                '''Gets and sets the diameter.'''
-                yield self.radius * 2
-                self.radius = (yield) / 2
-
-            @coroproperty
-            def circumference(self):
-                '''Gets and sets the circumference.'''
-                yield self.diameter * pi
-                self.diameter = (yield) / pi
-
-            @coroproperty
-            def area(self):
-                '''Gets and sets the area.'''
-                yield pow(self.radius, 2) * pi
-                self.radius = sqrt((yield) / pi)
-    """
-
-    def getter(self) -> Any:
-        """Property getter function."""
-        coro = method(self)
-        value = next(coro)
-        coro.close()
-        return value
-
-    def setter(self, value: Any):
-        """Property setter function."""
-        coro = method(self)
-        next(coro)
-        next(coro)
-
-        with suppress(StopIteration):
-            coro.send(value)
-
-    return property(getter, setter)
 
 
 def exiting(function: Callable) -> Callable:
--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 #! /usr/bin/env python3
 
-from distutils.core import setup
+from setuptools import setup
+
+DESCRIPTION = 'Even higher-order functions and operations on callable objects.'
 
 setup(
     name='functoolsplus',
-    version='latest',
+    use_scm_version={
+        "local_scheme": "node-and-timestamp"
+    },
+    setup_requires=['setuptools_scm'],
     author='HOMEINFO - Digitale Informationssysteme GmbH',
-    author_email='<info at homeinfo dot de>',
+    author_email='<info@homeinfo.de>',
     maintainer='Richard Neumann',
-    maintainer_email='<r dot neumann at homeinfo priod de>',
+    maintainer_email='<r.neumann@homeinfo.de>',
     py_modules=['functoolsplus'],
     license='GPLv3',
-    description='Even higher-order functions and'
-    ' operations on callable objects.')
+    description=DESCRIPTION
+)
